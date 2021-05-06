f401b695891fa2808436c0d8a44f067fcc6ddbee
Thomas-Briody <tom@briody.net>
2021-05-06 19:50:48 +0200

    tests set up for new constraints

--- spec/support/passwordTests.spec.js
+++ spec/support/passwordTests.spec.js
@@ -2,41 +2,58 @@
 
 const passwordVerifier = require('../../src/index')
 
-// import passwordVerifier from '../../src/index'
-//* anonymous functions being used to ensure function is called at the right time
-
 describe('Password Checks', function() {
-  it('fails if less than 8 characters', function() {
-    const tooShort = 'short'
-    expect(() => passwordVerifier(tooShort)).toThrowError('too short')
+  it('Fails if less than 8 characters', function() {
+    expect(() => passwordVerifier('short')).toThrowError('Insufficient')
   })
-  it('fails if null', function() {
-    const nullPassword = null
-    expect(() => passwordVerifier(nullPassword)).toThrowError('null!')
+  it('Fails if null', function() {
+    expect(() => passwordVerifier(null)).toThrowError('Password cannot be blank')
   })
-  it('fails if no lowercase characters', function() {
-    const noLowercase = 'HELLOSDSDSADA'
-    expect(() => passwordVerifier(noLowercase)).toThrowError('need a lowercase character!')
+  it('Fails if no lowercase characters', function() {
+    expect(() => passwordVerifier('HELLOOOOOOO1')).toThrowError('Password requires a lowercase character')
   })
-  it('fails if no uppercase characters', function() {
-    const noUppercase = 'hellosdsdsds'
-    expect(() => passwordVerifier(noUppercase)).toThrowError('need an uppercase character!')
+  it('Fails if no uppercase characters', function() {
+    expect(() => passwordVerifier('hello')).toThrowError('Insufficient')
   })
-  it('fails if no number', function() {
-    const noNumbers = 'Hello!long'
-    expect(() => passwordVerifier(noNumbers)).toThrowError('need a number!')
-  })
-  it('succeeds if all conditions are met', function() {
-    const result =  passwordVerifier('Passw0rd1')
-    expect(result).toBe(true)
+  it('Fails if no number', function() {
+    expect(() => passwordVerifier('hell')).toThrowError('Insufficient')
   })
   it('succeeds if 3 conditions are met', function() {
-    const result =  passwordVerifier('Aa1')
-    expect(result).toBe(true)
-  })
-  it('fails if 3 conditions are met but no lowercase', function() {
-    const testFunc = () => passwordVerifier('LONGPASS1')
-    expect(testFunc).toThrowError('requires a lowercase character!')
+    expect(passwordVerifier('aaaaaaaaaaa')).toBe(true)
   })
 })
 
+
+
+//* The test below is deemed irrelevant by the test to check if 3 conditions are met 
+
+// it('succeeds if all conditions are met', function() {
+//   const result =  passwordVerifier('Passw0rd1')
+//   expect(result).toBe(true)
+// })
+
+//* anonymous functions being used to ensure function is called at the right time
+//* Below is for basic implimentation with individual error messages. Unable to get this working with 
+//* additional requirements as can be multiple failed validations but only one error able to be thrown 
+
+// describe('Password Checks', function() {
+//   it('fails if less than 8 characters', function() {
+//     expect(() => passwordVerifier('Short1')).toThrowError('too short')
+//   })
+//   it('fails if null', function() {
+//     expect(() => passwordVerifier(null)).toThrowError('null!')
+//   })
+//   it('fails if no lowercase characters', function() {
+//     expect(() => passwordVerifier('HELLOSDSDSADA')).toThrowError('need a lowercase character!')
+//   })
+//   it('fails if no uppercase characters', function() {
+//     expect(() => passwordVerifier('hellosdsdsds')).toThrowError('need an uppercase character!')
+//   })
+//   it('fails if no number', function() {
+//     expect(() => passwordVerifier('HelloLong')).toThrowError('need a number!')
+//   })
+//   it('succeeds if all conditions are met', function() {
+//     const result =  passwordVerifier('Passw0rd1')
+//     expect(result).toBe(true)
+//   })
+// })
